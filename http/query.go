8f2afab2ce470bbf7d45fd9bbe5d5cb4cd1b7510
thisisaaronland <straup@gmail.com>
2018-04-12 06:43:57 -0700

    GetInt

--- http/query.go
+++ http/query.go
@@ -3,6 +3,7 @@ package http
 import (
 	"github.com/whosonfirst/go-sanitize"
 	gohttp "net/http"
+	"strconv"
 )
 
 func GetString(req *gohttp.Request, param string) (string, error) {
@@ -13,4 +14,15 @@ func GetString(req *gohttp.Request, param string) (string, error) {
 	raw_p := q.Get(param)
 
 	return sanitize.SanitizeString(raw_p, opts)
+}
+
+func GetInt(req *gohttp.Request, param string) (int, error) {
+
+	p, err := GetString(req, param)
+
+	if err != nil {
+		return -1, err
+	}
+
+	return strconv.Atoi(p)
 }
