43570534b75e10e851592b1f3e31d87064b9cb96
thisisaaronland <straup@gmail.com>
2018-04-26 08:44:29 -0700

    drop salt from users table; signup seems to work but cookie check fails

--- tables/users.go
+++ tables/users.go
@@ -48,10 +48,8 @@ func (t *UsersTable) Schema() string {
 	    	id INTEGER PRIMARY KEY AUTOINCREMENT,
 		name TEXT NOT NULL,
 		email TEXT NOT NULL,		
-		password TEXT NOT NULL,
-		salt TEXT NOT NULL
+		password TEXT NOT NULL
 	);
-
 	`
 
 	return fmt.Sprintf(sql, t.Name())
