3db15e935f27a44e5293612d6391957232f81c9c
thisisaaronland <straup@gmail.com>
2018-04-30 08:57:09 -0700

    start fleshing out crumb stuff

--- crumb/crumb.go
+++ crumb/crumb.go
@@ -1,23 +1,42 @@
 package crumb
 
 import (
-	"fmt"
+	"errors"
+	"github.com/aaronland/go-secretbox"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 )
 
 var sep string = "-"
 
-func GenerateCrumb(req *http.Request, extra ...string) (string, error) {
+type CrumbConfig struct {
+	Snowman string
+	Salt    string
+	Secret  string
+}
 
-	crumb_base, err := CrumbBase(req, extra...)
+func NewCrumbConfig() CrumbConfig {
+
+	cfg := CrumbConfig{
+		Snowman: "SNOWMAN",
+		Salt:    "salt",
+		Secret:  "secret",
+	}
+
+	return cfg
+}
+
+func GenerateCrumb(req *http.Request, cfg CrumbConfig, extra ...string) (string, error) {
+
+	crumb_base, err := CrumbBase(req, cfg, extra...)
 
 	if err != nil {
 		return "", err
 	}
 
-	crumb_hash, err := HashCrumb(crumb_base)
+	crumb_hash, err := HashCrumb(cfg, crumb_base)
 
 	if err != nil {
 		return "", err
@@ -28,39 +47,59 @@ func GenerateCrumb(req *http.Request, extra ...string) (string, error) {
 	crumb_parts := []string{
 		strconv.Itoa(time.Unix()),
 		crumb_hash,
-		"SNOWMAN",
+		cfg.Snowman,
 	}
 
 	crumb_var := strings.Join(crumb_parts, sep)
 	return crumb_var, nil
 }
 
-func ValidateCrumb(req *gohttp.Request, crumb_var string, ttl int, extra ...string) (bool, error) {
+func ValidateCrumb(req *http.Request, cfg CrumbConfig, crumb_var string, ttl int, extra ...string) (bool, error) {
 
 	crumb_parts := strings.Split(crumb_var, sep)
 
 	if ttl > 0 {
 
+		then, err := strconv.Atoi(crumb_parts[0])
+
+		if err != nil {
+			return false, err
+		}
+
+		now := time.Now()
+		ts := now.Unix()
+
+		if now-then > ttl {
+			return false, errors.New("Crumb has expired")
+		}
 	}
 
-	crumb_base, err := CrumbBase(req, extra...)
+	crumb_base, err := CrumbBase(req, cfg, extra...)
 
 	if err != nil {
 		return false, err
 	}
 
-	crumb_hash, err := CrumbHash(crumb_base)
+	crumb_hash, err := HashCrumb(cfg, crumb_base)
+
+	if len(crumb_hash) != len(crumb_var) {
+		return false, errors.New("Invalid crumb")
+	}
+
+	if crumb_hash != crumb_var {
+		return false, errors.New("Invalid crumb")
+	}
 
 	return true, nil
 }
 
-func CrumbKey(req *http.Request) string {
+func CrumbKey(req *http.Request, cfg CrumbConfig) string {
 	return req.URL.Path
 }
 
-func CrumbBase(req *http.Request, key string, extra ...string) (string, error) {
+func CrumbBase(req *http.Request, cfg CrumbConfig, extra ...string) (string, error) {
 
-	crumb_key := CrumbKey(req)
+	crumb_key := CrumbKey(req, cfg)
 
 	base := make([]string, 0)
 
@@ -75,7 +114,18 @@ func CrumbBase(req *http.Request, key string, extra ...string) (string, error) {
 	return str_base, nil
 }
 
-func HashCrumb(raw string) (string, error) {
+func HashCrumb(cfg CrumbConfig, raw string) (string, error) {
 
-	return raw, nil
+	opts := secretbox.NewSecretboxOptions()
+	opts.Salt = cfg.Salt
+
+	sb, err := secretbox.NewSecretbox(cfg.Secret, opts)
+
+	enc, err := sb.Lock([]byte(raw))
+
+	if err != nil {
+		return "", err
+	}
+
+	return enc, nil
 }
